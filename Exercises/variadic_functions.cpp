3e5d592c2124f1af1f9527011d49071436a833b1
MZhikoV <m.zhikov@gmail.com>
2024-10-03 09:14:56 +0300

    ariadic function practice

--- Exercises/variadic_functions.cpp
+++ Exercises/variadic_functions.cpp
@@ -1,68 +1,131 @@
 #include <iostream>
 #include <cstdarg>
 
-int sum(int count,...) {
-    int total=0;
+// int sum(int count,...) {
+//     int total=0;
+
+//     std::va_list args;
+//     va_start(args,count); //initialise the argument list
+
+//     for (int i=0; i<count; i++) {
+//         int v=va_arg(args,int);
+//         std::cout<<v<<std::endl;
+//         total+=v;
+//     };
+
+//     va_end(args);
+
+//     return total;   
+// };
+
+// int max (int count,...) {
+//     int maxEl=INT_MIN;
+//     std::va_list args;
+//     va_start(args,count);
+
+//     for (int i=0; i<count;i++) {
+//         int currArg=va_arg(args,int);
+
+//         if (maxEl<currArg) {
+//             maxEl=currArg;
+//         };
+//     };
+
+//     va_end(args);
+
+//     return maxEl;
+// }
+
+// int min(int count,...) {
+//     int minEl=INT_MAX;
+
+//     std::va_list args;
+//     va_start(args,count);
+
+//     for (int i=0; i<count;i++) {
+//         int currEl=va_arg(args,int);
+//         if (currEl<minEl) {
+//             minEl=currEl;
+//         };
+//     };
+
+//     va_end(args);
+//     return minEl;
+// };
+
+int sum(int count, ...) {
+    int result=0;
 
     std::va_list args;
     va_start(args,count); //initialise the argument list
 
     for (int i=0; i<count; i++) {
         int v=va_arg(args,int);
-        std::cout<<v<<std::endl;
-        total+=v;
+        std::cout<<v<<' ';
+        result+=v;
     };
 
     va_end(args);
 
-    return total;   
+    std::cout<<std::endl;
+
+    return result;
 };
 
-int max (int count,...) {
-    int maxEl=INT_MIN;
+int max(int count, ...) {
+    int result=INT_MIN;
+
     std::va_list args;
-    va_start(args,count);
 
-    for (int i=0; i<count;i++) {
-        int currArg=va_arg(args,int);
+    va_start(args,count); //initialise argument list
 
-        if (maxEl<currArg) {
-            maxEl=currArg;
+    for (int i=0; i<count; i++) {
+        int v=va_arg(args,int);
+        std::cout<<v<<' ';
+        if (result<v) {
+            result=v;
         };
     };
 
     va_end(args);
 
-    return maxEl;
+    std::cout<<std::endl;
+
+    return result;
+};
+
+int min(int count, ...) {
+    int result=INT_MAX;
+
+    std::va_list(args); //initialise arguments list
+
+    va_start(args,count);
+
+    for (int i=0; i<count; i++) {
+        int v=va_arg(args, int);
+        std::cout<<v<<' ';
+        if (result>v) {
+            result=v;
+        };
+    };
+
+    va_end(args);
+
+    std::cout<<std::endl;
+
+    return result;
 }
-
-int min(int count,...) {
-    int minEl=INT_MAX;
-
-    std::va_list args;
-    va_start(args,count);
-
-    for (int i=0; i<count;i++) {
-        int currEl=va_arg(args,int);
-        if (currEl<minEl) {
-            minEl=currEl;
-        };
-    };
-
-    va_end(args);
-    return minEl;
-};
 
 int main() {
 
     int result=sum(4, 10, 20, 30, 40);
-    std::cout<<result<<std::endl;
+    std::cout<<"Sum of elements: "<<result<<std::endl<<std::endl;
 
     int maxel=max(5, 1, 420, 16, 30, 940);
-    std::cout<<maxel<<std::endl;
+    std::cout<<"Max element is: "<<maxel<<std::endl<<std::endl;
 
     int minel=min(5, 1, 420, 0, 30, 940);
-    std::cout<<minel<<std::endl;
+    std::cout<<"Min element is: "<<minel<<std::endl<<std::endl;
 
 
     system("pause");
